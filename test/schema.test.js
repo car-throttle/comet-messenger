ba5e3664c5140a656cf79678d7dc750af7d3af83
jdrydn <james@jdrydn.com>
2017-04-27 12:11:52 +0100

    Fixing test
    It's funny, the one test I wrote I broke.
    :clap:

--- test/schema.test.js
+++ test/schema.test.js
@@ -8,12 +8,10 @@ describe('Comet-Messenger Schema', function () {
 
     assert.equal(typeof schema, 'object');
     assert.equal(typeof schema._getFunction, 'function');
-    assert.equal(typeof schema._hasFunction, 'function');
     assert.equal(typeof schema.catchInput, 'function');
     assert.equal(typeof schema.onInput, 'function');
     assert.equal(typeof schema.onPostback, 'function');
-    assert.equal(typeof schema.getUserProfile, 'function');
-    assert.equal(typeof schema.getUserState, 'function');
-    assert.equal(typeof schema.setUserState, 'function');
+    assert.equal(typeof schema.before, 'function');
+    assert.equal(typeof schema.after, 'function');
   });
 });
